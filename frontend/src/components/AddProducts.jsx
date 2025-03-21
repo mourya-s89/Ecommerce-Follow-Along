66b838671911efec50c46f8c23ab72a197bd9909
mourya-s89 <kadali.mourya.s89@kalvium.community>
2025-03-21 15:24:14 +0530

    files

--- frontend/src/components/AddProducts.jsx
+++ frontend/src/components/AddProducts.jsx
@@ -1,19 +1,46 @@
 import React from 'react'
 import { useState } from 'react'
+import axios from 'axios'
 
-const AddProducts = () => {
+const AddProduct = () => {
     const [noOfImages,setNoOfImages] = useState(new Array(1));
     const [productDetails,setProductDetails]=useState({
         name:"",
         email:"",
-        password:""
-    });
-    const [productImages,setProductImages]
+        password:"",        
+    })
+    const [productImages,setProductImages]=useState([])
+
+    async function handelSubmit(e){
+        try{
+            const {name,email,password} = productDetails;
+            if(!name||!email||!password || productImages.length ==0){
+                alert("add all fields")
+                return;
+            }
+            const formData = new FormData();
+            await axios.post("http://localhost:5173/product/AddProducts")
+             
+         }catch(err){
+            console.log(err);
+            alert("something went  wrong while sending data")
+        }
+
+    }
+
   return (
     <div>
         <form action="">
-            <input type="text" name={"title"} placeholder='Enter Title....' />
-            <input type="text" name={"description"} placeholder='Enter Product description....' />
+            <input type="text" name={"title"} placeholder='Enter Title....' onChange={(event)=>{
+                setProductDetails({...productDetails,[event.target.name]:event.target.value})
+            }} />
+            <input type="text" name={"description"} placeholder='Enter Product description....' onChange={(event)=>{
+                setProductDetails({...productDetails,[event.target.name]:event.target.value})
+            }} />
+            <input type="number" name="price" placeholder='Enter price...' onChange={(event)=>{
+                    setProductDetails({...productDetails,[event.target.name]:event.target.value})
+                }}
+             />
             <select name="" id="" onChange={(event)=>{
                 console.log(event.target.value,noOfImages)
                 setNoOfImages(new Array(parseInt(event.target.value)).fill(1))
@@ -30,7 +57,9 @@ const AddProducts = () => {
             <label htmlFor="">Add Images</label>
             {
                 noOfImages.map((ele)=>{
-                    <input type='file' accept='image/*'></input>
+                    <input type='file' accept='image/*' onChange={(event)=>{
+                        setProductImages([...productImages,event.target.files[0]])
+                    }}></input>
                     
                 })
             }
@@ -40,4 +69,4 @@ const AddProducts = () => {
   )
 }
 
-export default AddProducts
+export default AddProduct
